ef7143c06fc9007deb2322466cde8bfac4dff0c6
mrsondeptrai1999 <mrsondeptrai1999@gmail.com>
2019-07-22 13:51:49 +0100

    22/7/2019

--- read_and_write.R
+++ read_and_write.R
@@ -1,21 +1,19 @@
 my_read <- function(directory) {
   
   df = read.table(file = directory, sep = '\t', header = FALSE)
-  write.table(df[1,-1],'field_id.csv',sep=',',row.names = FALSE,col.names = FALSE)
-  write.table(df[,1],'subject_id.csv',sep=',',row.names = FALSE,col.names = FALSE)
-  write.table(df[-1,-1],'value.csv',sep=',',row.names = FALSE,col.names = FALSE)
+  write.table(df[1,-1],'field_id_0.csv',sep=',',row.names = FALSE,col.names = FALSE)
+  write.table(df[,1],'subject_id_0.csv',sep=',',row.names = FALSE,col.names = FALSE)
+  write.table(df[,-1],'value_0.csv',sep=',',row.names = FALSE,col.names = FALSE)
   
 }
 
-my_read('Conf_Variable.tsv')
 
 my_write <- function(field_id,subject_id,value,directory) {
   df1 = read.table(file = field_id, sep = ',', header = FALSE)
   df2 = read.table(file = subject_id, sep = ',', header = FALSE)
   df3 = read.table(file = value, sep = ',', header = FALSE)
   df3[] <- sapply(df3,as.character) 
-  df4 = rbind(df1,df3)
-  df5 = cbind(df2, df4)
+  #df4 = rbind(df1,df3)
+  df5 = cbind(df2, df3)
   write.table(df5,directory,sep=',',row.names = FALSE,col.names = FALSE)
 }
-my_write('field_id.csv','subject_id.csv','value.csv', 'output.csv')
