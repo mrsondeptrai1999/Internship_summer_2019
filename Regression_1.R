e4fb425aee58aa70d51bd6f17f8b087560dcef85
mrsondeptrai1999 <mrsondeptrai1999@gmail.com>
2019-08-05 12:06:20 +0100

    5/8/19

--- Regression_1.R
+++ Regression_1.R
@@ -66,6 +66,56 @@ print('conf')
 
 
 # DATA CLEANING FOR GEOSPATIAL VARIABLES--------------------------------
+df1 <- read.table('my_data/Geo_Variable_Visit_0_0.tsv',sep='\t',header=TRUE)
+df2 <- read.table('my_data/Geo_Variable_2.tsv',sep='\t',header=TRUE)
+# did not download everything in first file so have to download the missing data is second file
+#df3 <- read.table('geo_table_ver_2.csv',sep=',',header=TRUE)
+
+df2_2 <- df2[-c(13:15)] # these field _id unpacked in first file so delete
+df2_2 <- df2[-c(6:8)] # remove 24016,24017,24108 since we have data in 2010
+
+df2_particulate <- df2[c(2:5)] # Deal with particulate matter fields
+df2_particulate_2 <- mean_impute(df2_particulate) # mean impute to calculate corr
+cor(df2_particulate_2)
+
+# get this
+'           X24005.0.0 X24007.0.0 X24006.0.0 X24008.0.0
+X24005.0.0  1.0000000  0.5081112  0.4921478  0.7924319
+X24007.0.0  0.5081112  1.0000000  0.5274889  0.3675723
+X24006.0.0  0.4921478  0.5274889  1.0000000  0.1416952
+X24008.0.0  0.7924319  0.3675723  0.1416952  1.0000000'
+# 05,08,07 seems to have high correlation to we average them out
+# 06's correlation values near 0 so we dont average them
+
+df2_3 <- df2_2
+df2_3[2] <- (df2_3[2]+df2_3[3]+df2_3[5])/3
+df2_3 <- df2_3[-c(3,5)] # average out 24005,07,08
+
+df2_nitrogen <- df2_3[c(4,5)]
+df2_nitrogen_2 <- mean_impute(df2_nitrogen) # mean impute to calculate corr
+cor(df2_nitrogen_2)
+'           X24003.0.0 X24004.0.0
+X24003.0.0  1.0000000  0.9141271
+X24004.0.0  0.9141271  1.0000000'
+# high corr between those two so we average them out
+
+df2_4 <- df2_3
+df2_4[4] <- (df2_4[4] + df2_4[5])/2
+df2_4<- df2_4[-5]
+df2_4<- df2_4[-5] #delete 24019
+
+df2_5 <- df2_4[-c(10,12,14,16,18,20,22,24)] #delete second visit since I forgot to flag -v 0 in funpack
+# average 24506,07 ; 00,03 ; 01,04 ; 02,05
+for (i in 4:7){
+  df2_5[2*i+1] <- (df2_5[2*i+1] + df2_5[2*i+2])/2
+} 
+df2_5 <- df2_5[-c(10,12,14,16)] 
+# rename cols
+colnames(df2_5)[c(2,9,10,11,12)] <- c("X24005_07_08.0.0","X24506_07.0.0","X24500_03.0.0","X24501_04.0.0","X24502_05.0.0")
+df2_6 <- cbind(df1,df2_5[-1])
+
+write.table(df2_6,'my_data/geo_table_ver_2.csv',sep=',',row.names = FALSE)
+print('geo')
 
 #----------------------------------------------------------------
 
